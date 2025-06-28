76e32a409781b1f6d7b00b385f6aafc332e185ae
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-06-28 20:20:49 +0530

    index

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import http from 'http'
+import app from "./app"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+    console.log(`connected to: http://localhost:${PORT}`)
+})
