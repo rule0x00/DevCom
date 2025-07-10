6e731bf6604253c454cfdf197319227f7daa84a1
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-07-10 20:32:07 +0530

    user route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,31 +1,9 @@
 import express from 'express'
-import { Request, Response } from 'express'
-import userModel from '../models/users'
+import userSignup from '../controllers/signup'
+import userLogin from '../controllers/userLogin'
 const router = express.Router()
 
-router.post("/signup", async (req: Request, res: Response) => {
-    const {user_name, email, password} = req.body
-
-    if(!user_name || !email || !password){
-        res.status(400).send("Missing curcial information")
-    }
-    try{
-        const user = await userModel.create({
-            user_name: user_name,
-            email: email,
-            password: password
-        })
-    
-        res.status(201).json({"msg": `User succesfully created for ${user_name}`, "data": user})
-        res.redirect("/")
-    }catch(err){
-        if (err instanceof Error) {
-            res.status(500).json({ error: err.message }); //Type safety bypass
-          } else {
-            res.status(500).json({ error: "Unknown error occurred" });
-          }
-    }
-})
-
+router.post("/signup",userSignup)
+router.post("/login", userLogin)
 
 export default router
