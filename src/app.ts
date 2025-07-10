e9e286f3a227c17602ca36bdb63e0bf1e95fbe09
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-07-10 20:32:22 +0530

    app

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,29 @@ import express, { urlencoded } from "express"
 import morgan from 'morgan'
 import cors from 'cors'
 import connection from "./utils/connect"
-import router from "./routes/user"
+import userRouter from "./routes/user"
+import blogRouter from './routes/blogs'
+import cookieParser from 'cookie-parser'
+import { authenticateUser } from "./middleware/authenticate"
 
 
 const app = express()
 
+//Middlewares
 app.use(morgan('dev'));
 app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cookieParser())
+app.use(authenticateUser("token"))
 
+//Routes
+app.use("/user", userRouter)
+app.use("/blogs", blogRouter)
 
-app.use("/", router)
-
+app.use("/home", (req, res) => {
+    res.status(200).send("Welcome to DevCom. A Blogging application for lonely developers. Yeah you heard me.")
+})
 
 connection()
 
